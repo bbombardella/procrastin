ff004466f04efc601c97903ac2198b7e7ebb9df0
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2025-02-05 11:29:21 +0100

    chore(ui): center sign in form

--- apps/mobile/app/(public)/sign-in.tsx
+++ apps/mobile/app/(public)/sign-in.tsx
@@ -13,11 +13,16 @@ import {
     FormControlLabelText
 } from '../../components/ui/form-control'
 import {Button, ButtonText} from '../../components/ui/button'
-import {Input, InputField} from '../../components/ui/input'
+import {Input, InputField, InputIcon, InputSlot} from '../../components/ui/input'
 import {VStack} from '../../components/ui/vstack'
 import {Center} from '../../components/ui/center';
 import {Divider} from '../../components/ui/divider';
 import {Link} from 'expo-router';
+import {Text} from '../../components/ui/text';
+import {Heading} from '../../components/ui/heading';
+import {View} from 'react-native';
+import {useState} from 'react';
+import {EyeIcon, EyeOffIcon} from '../../components/ui/icon';
 
 const formSchema = z.object({
     email: z.string().email('Please enter a valid email address.'),
@@ -38,6 +43,8 @@ export default function SignIn() {
         },
     })
 
+    const [showPassword, setShowPassword] = useState(false)
+
     async function onSubmit(data: z.infer<typeof formSchema>) {
         try {
             await signInWithPassword(data.email, data.password)
@@ -49,83 +56,94 @@ export default function SignIn() {
     }
 
     return (
-        <VStack className="w-full p-4">
-            <Controller
-                control={form.control}
-                name="email"
-                render={({field: {onChange, onBlur, value}}) => (
-                    <FormControl
-                        isInvalid={!!form.formState.errors.email}
-                        size="md"
-                        isRequired={true}
-                    >
-                        <FormControlLabel>
-                            <FormControlLabelText>Email</FormControlLabelText>
-                        </FormControlLabel>
-                        <Input className="my-1">
-                            <InputField
-                                type="text"
-                                placeholder="Your email"
-                                value={value}
-                                onChangeText={onChange}
-                                onBlur={onBlur}
-                            />
-                        </Input>
-                        <FormControlError>
-                            <FormControlErrorText>
-                                {form.formState.errors.email?.message}
-                            </FormControlErrorText>
-                        </FormControlError>
-                    </FormControl>
-                )}
-            />
+        <VStack className="w-full h-full p-4 justify-center">
+            <Heading className="text-center my-4" size="5xl">ProcrastIn</Heading>
 
-            <Controller
-                control={form.control}
-                name="password"
-                render={({field: {onChange, onBlur, value}}) => (
-                    <FormControl
-                        className="mt-2"
-                        isInvalid={!!form.formState.errors.password}
-                        size="md"
-                        isRequired={true}
-                    >
-                        <FormControlLabel>
-                            <FormControlLabelText>Password</FormControlLabelText>
-                        </FormControlLabel>
-                        <Input className="my-1">
-                            <InputField
-                                type="password"
-                                placeholder="Your password"
-                                value={value}
-                                onChangeText={onChange}
-                                onBlur={onBlur}
-                            />
-                        </Input>
-                        <FormControlHelper>
-                            <FormControlHelperText>
-                                Must be at least 6 characters.
-                            </FormControlHelperText>
-                        </FormControlHelper>
-                        <FormControlError>
-                            <FormControlErrorText>
-                                {form.formState.errors.password?.message}
-                            </FormControlErrorText>
-                        </FormControlError>
-                    </FormControl>
-                )}
-            />
+            <Heading className="text-center my-4 mb-8" size="lg">Please sign in to access the application.</Heading>
 
-            <Center>
-                <Button className="w-full mt-4" size="md" onPress={form.handleSubmit(onSubmit)}>
-                    <ButtonText>Login</ButtonText>
-                </Button>
-                <Divider className="my-4"/>
+            <View>
+                <Controller
+                    control={form.control}
+                    name="email"
+                    render={({field: {onChange, onBlur, value}}) => (
+                        <FormControl
+                            isInvalid={!!form.formState.errors.email}
+                            size="md"
+                            isRequired={true}
+                        >
+                            <FormControlLabel>
+                                <FormControlLabelText>Email</FormControlLabelText>
+                            </FormControlLabel>
+                            <Input className="my-1" size="lg">
+                                <InputField
+                                    type="text"
+                                    placeholder="Your email"
+                                    value={value}
+                                    onChangeText={onChange}
+                                    onBlur={onBlur}
+                                />
+                            </Input>
+                            <FormControlError>
+                                <FormControlErrorText>
+                                    {form.formState.errors.email?.message}
+                                </FormControlErrorText>
+                            </FormControlError>
+                        </FormControl>
+                    )}
+                />
 
-                <Button className="w-full" size="md" variant="outline" onPress={form.handleSubmit(onSubmit)}>
-                    <Link href="/sign-up">Sign up</Link>
-                </Button>
-            </Center>
+                <Controller
+                    control={form.control}
+                    name="password"
+                    render={({field: {onChange, onBlur, value}}) => (
+                        <FormControl
+                            className="mt-2"
+                            isInvalid={!!form.formState.errors.password}
+                            size="md"
+                            isRequired={true}
+                        >
+                            <FormControlLabel>
+                                <FormControlLabelText>Password</FormControlLabelText>
+                            </FormControlLabel>
+                            <Input className="my-1" size="lg">
+                                <InputField
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder="Your password"
+                                    value={value}
+                                    onChangeText={onChange}
+                                    onBlur={onBlur}
+                                />
+                                <InputSlot className="pr-3" onPress={() => setShowPassword(!showPassword)}>
+                                    <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+                                </InputSlot>
+                            </Input>
+                            <FormControlHelper>
+                                <FormControlHelperText>
+                                    Must be at least 6 characters.
+                                </FormControlHelperText>
+                            </FormControlHelper>
+                            <FormControlError>
+                                <FormControlErrorText>
+                                    {form.formState.errors.password?.message}
+                                </FormControlErrorText>
+                            </FormControlError>
+                        </FormControl>
+                    )}
+                />
+
+                <Center>
+                    <Button className="w-full mt-4" size="md" onPress={form.handleSubmit(onSubmit)}>
+                        <ButtonText>Sign in</ButtonText>
+                    </Button>
+                    <Divider className="my-4"/>
+
+                    <Button className="w-full" size="md" variant="outline" onPress={form.handleSubmit(onSubmit)}>
+                        <Link href="/sign-up">
+                            <Text>Sign up</Text>
+                        </Link>
+                    </Button>
+                </Center>
+            </View>
         </VStack>
     )
 }
