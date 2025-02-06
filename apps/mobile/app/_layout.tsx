80fb6e0897308352a45d4015bd9c551835897277
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2025-02-06 17:25:09 +0100

    fix(apps/mobile): status bar color scheme

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout() {
                                 <Stack.Screen name="(protected)" options={{headerShown: false}}/>
                                 <Stack.Screen name="+not-found"/>
                             </Stack>
-                            <StatusBar style="auto"/>
+                            <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'}/>
                         </SafeAreaProvider>
                     </SupabaseProvider>
                 </QueryClientProvider>
