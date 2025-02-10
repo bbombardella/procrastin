0b123696a40b8a71709e7108686fd645362d01d1
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2025-02-10 11:14:17 +0100

    chore(ui): add date to comment

--- apps/mobile/components/CommentView.tsx
+++ apps/mobile/components/CommentView.tsx
@@ -3,8 +3,11 @@ import { Avatar, AvatarFallbackText, AvatarImage } from "./ui/avatar";
 import { Box } from "./ui/box";
 import { HStack } from "./ui/hstack";
 import { Text } from "./ui/text";
+import {useMemo} from 'react';
+import {VStack} from './ui/vstack';
 
 export default function CommentView({ comment }: {comment: CommentWithAuthor}) {
+    const formattedDate = useMemo(() => new Date(comment.createdAt).toLocaleString(), [comment.createdAt])
 
     return (
         <Box className="p-4 gap-2 background-primary-500">
@@ -17,7 +20,10 @@ export default function CommentView({ comment }: {comment: CommentWithAuthor}) {
                         }}
                     />
                 </Avatar>
-                <Text>{comment.author.firstName}&nbsp;{comment.author.lastName}</Text>
+                <VStack>
+                    <Text>{comment.author.firstName}&nbsp;{comment.author.lastName}</Text>
+                    <Text size="sm">{formattedDate}</Text>
+                </VStack>
             </HStack>
             <Text>{comment.content}</Text>
         </Box>
